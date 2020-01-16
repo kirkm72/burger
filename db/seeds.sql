a730985d9dbfae7cf929d32ff9336ab10f79d4e6
Kirk McGlamery <kirkm72@att.net>
2020-01-16 15:46:39 -0600

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Single meat');
+INSERT INTO burgers (name) VALUES ('Double meat');
+INSERT INTO burgers (name, eaten) VALUES ('Double meat cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baconburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Chicken Sandwich', true);
