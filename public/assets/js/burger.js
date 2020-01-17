d4e1ab8135ca77b2612330121081759d8fc9710d
Kirk McGlamery <kirkm72@att.net>
2020-01-17 15:49:39 -0600

    updated FE JS to sync with handlebars ID's & classes

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,40 +1,54 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-devoured").on("click", function(event) {
-    let id = $(this).data("id");
-    let newDevoured = $(this).data("newDevoured");
+$(function () {
+  $(".devourIt").on("click", function (event) {
+    event.preventDefault();
 
-    let newDevourState = {
-      devoured: newDevoured
+    var id = $(this).data("id");
+    var updateBurger = {
+      id: $(this).data("id"),
+      devoured: true
     };
 
-    $.ajax("/api/burgers/" + id, { //PUT request.
+    $.ajax("/api/burgers/" + id, { //PUT request
       type: "PUT",
-      data: newDevouredState
-    }).then(
-      function() {
-        console.log("changed devoured to", newDevoured);
+      data: updateBurger
+    }).then(function () {
+        console.log("changed devoured to", updateBurger.devoured);
         location.reload(); //Page reload for updates
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $("#createForm").on("submit", function (event) {
     event.preventDefault(); //preventDefault needed on a submit event.
 
     let newBurger = {
       burgerName: $("#burger-name").val().trim(),
+      devoured: false
     };
 
     $.ajax("/api/burgers", { //POST request.
       type: "POST",
       data: newBurger
     }).then(
-      function() {
+      function () {
         console.log("created new burger");
-        
         location.reload(); //Page reload for updates
       }
     );
+  });
+  $(".deleteIt").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    console.log("Delete button clicked for id# ", id);
+
+    $.ajax({ //DELETE request.
+      url: "/api/burgers/" + id,
+      type: "DELETE"
+    }).then(function(response) {
+      console.log("Database deletion for burger# ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 });
