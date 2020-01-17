a6e85c5e4674c7c99ec900d8e6ce18fee08020e5
Kirk McGlamery <kirkm72@att.net>
2020-01-17 11:09:20 -0600

    fixed inconsistencies

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-let connection = require("./connection.js"); //use this connection
+const connection = require("./connection.js"); //use this connection
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
@@ -7,7 +7,7 @@ let connection = require("./connection.js"); //use this connection
 function printQuestionMarks(num) {
   let arr = [];
 
-  for (var i = 0; i < num; i++) {
+  for (let i = 0; i < num; i++) {
     arr.push("?");
   }
   return arr.toString();
@@ -32,7 +32,7 @@ function objToSql(ob) { // Helper function to convert object key/value pairs to 
 }
 
 let orm = { // Object for all our SQL statement functions.
-  all: function(tableInput, cb) {
+  selectAll: function(tableInput, cb) {
     let queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
@@ -41,15 +41,16 @@ let orm = { // Object for all our SQL statement functions.
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  createOne: function(table, cols, vals, cb) {
     let queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+    // queryString += " (";
+    // queryString += cols.toString();
+    // queryString += ") ";
+    // queryString += "VALUES (";
+    // queryString += printQuestionMarks(vals.length);
+    // queryString += ") ";
+    queryString += ` (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`
 
     console.log(queryString);
 
@@ -62,13 +63,14 @@ let orm = { // Object for all our SQL statement functions.
     });
   },
   
-  update: function(table, objColVals, condition, cb) { //example of objColVals would be {burgerName: Baconburger, devoured: true}
+  updateOne: function(table, objColVals, condition, cb) { //example of objColVals would be {burgerName: Baconburger, devoured: true}
     var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+    // queryString += " SET ";
+    // queryString += objToSql(objColVals);
+    // queryString += " WHERE ";
+    // queryString += condition;
+    queryString += ` SET ${objToSql(objColVals)} WHERE ${condition}`;
 
     console.log(queryString);
     connection.query(queryString, function(err, result) {
@@ -79,19 +81,19 @@ let orm = { // Object for all our SQL statement functions.
       cb(result);
     });
   },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
+  // delete: function(table, condition, cb) {
+  //   var queryString = "DELETE FROM " + table;
+  //   queryString += " WHERE ";
+  //   queryString += condition;
 
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+  //   connection.query(queryString, function(err, result) {
+  //     if (err) {
+  //       throw err;
+  //     }
 
-      cb(result);
-    });
-  }
+  //     cb(result);
+  //   });
+  // }
 };
 
 module.exports = orm; // Export the orm object for the model (burger.js).
